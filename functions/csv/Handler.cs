9601a9721ca2c9719c600526c4989a504317a8d6
shauge2206 <shauge1@gmail.com>
2024-10-08 22:59:58 +0200

    cleanup, functionality done in kaggle and csv lambda

--- functions/csv/Handler.cs
+++ functions/csv/Handler.cs
@@ -6,8 +6,6 @@ using CsvHelper;
 using CsvHelper.Configuration;
 using System.IO.Compression;
 
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
-
 namespace functions.csv
 {
   public class HandlerFunction
@@ -29,6 +27,7 @@ namespace functions.csv
       s3Client = new AmazonS3Client();
     }
 
+
     public async Task<string> DataCleansing()
     {
       GetObjectResponse s3Response = await GetFileS3(InputFileName);
@@ -38,52 +37,35 @@ namespace functions.csv
       MemoryStream outputFile = await CompressStreamToZip(modifiedCsvStream, newCsvFileName);
 
       return await PutFileS3(outputFile, OutputFileName);
-
     }
+
 
     public async Task<GetObjectResponse> GetFileS3(string fileName)
     {
-      try
-      {
-        var request = new GetObjectRequest
-        {
-          BucketName = S3BucketName,
-          Key = fileName,
-        };
+      GetObjectRequest request = new() { BucketName = S3BucketName, Key = fileName };
 
-        return await s3Client.GetObjectAsync(request);
-      }
-      catch (AmazonS3Exception ex)
-      {
-        Console.WriteLine($"Error encountered while reading S3 bucket. Message: {ex.Message}");
-        throw;
-      }
+      return await s3Client.GetObjectAsync(request);
     }
 
+
+    // Proabably able to combine with CopyContent.. method in kaggle Lambda
     public async Task<MemoryStream> S3FileToMemoryStream(GetObjectResponse response)
     {
-      try
-      {
-        var memoryStream = new MemoryStream();
-        await response.ResponseStream.CopyToAsync(memoryStream);
-        memoryStream.Position = 0;
-        return memoryStream;
-      }
-      catch (Exception ex)
-      {
-        Console.WriteLine($"Error encountered while converting S3 file to MemoryStream. Message: {ex.Message}");
-        throw;
-      }
+      MemoryStream memoryStream = new();
+      await response.ResponseStream.CopyToAsync(memoryStream);
+      memoryStream.Position = 0;
+      return memoryStream;
     }
+
 
     public async Task<MemoryStream> ModifyCsv(MemoryStream stream)
     {
       stream.Position = 0;
 
-      using var reader = new StreamReader(stream);
-      using var csvReader = new CsvReader(reader, new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture));
+      using StreamReader reader = new(stream);
+      using CsvReader csvReader = new(reader, new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture));
 
-      var movies = csvReader.GetRecords<dynamic>().ToList();
+      List<dynamic> movies = csvReader.GetRecords<dynamic>().ToList();
 
       foreach (var row in movies)
       {
@@ -91,8 +73,8 @@ namespace functions.csv
       }
 
       MemoryStream modifiedStream = new();
-      using var writer = new StreamWriter(modifiedStream, Encoding.UTF8, leaveOpen: true);
-      using var csvWriter = new CsvWriter(writer, new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture));
+      using StreamWriter writer = new(modifiedStream, Encoding.UTF8, leaveOpen: true);
+      using CsvWriter csvWriter = new(writer, new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture));
       csvWriter.WriteRecords(movies);
 
       await writer.FlushAsync();
@@ -108,7 +90,7 @@ namespace functions.csv
       zipStream.Position = 0;
       MemoryStream extractedFileStream = new();
 
-      using (var archive = new ZipArchive(zipStream, ZipArchiveMode.Read, leaveOpen: true))
+      using (ZipArchive archive = new(zipStream, ZipArchiveMode.Read, leaveOpen: true))
       {
         var entry = archive.GetEntry(fileToBeExtracted) ?? throw new FileNotFoundException($"File '{ZipFileExtract}' not found in the ZIP archive.");
 
@@ -120,48 +102,37 @@ namespace functions.csv
       return extractedFileStream;
     }
 
+
     private async Task<string> PutFileS3(Stream stream, string fileName)
     {
-      try
+      PutObjectRequest request = new()
       {
-        var request = new PutObjectRequest
-        {
-          BucketName = S3BucketName,
-          Key = fileName,
-          InputStream = stream,
-          ContentType = "application/zip"
-        };
+        BucketName = S3BucketName,
+        Key = fileName,
+        InputStream = stream,
+        ContentType = "application/zip"
+      };
 
-        PutObjectResponse response = await s3Client.PutObjectAsync(request);
+      await s3Client.PutObjectAsync(request);
 
-        Console.WriteLine($"Successfully uploaded modified CSV to '{S3BucketName}'.");
-        return $"Successfully uploaded modified CSV to '{S3BucketName}'.";
-      }
-      catch (AmazonS3Exception ex)
-      {
-        Console.WriteLine($"Error uploading modified CSV file to S3: {ex.Message}");
-        throw;
-      }
+      Console.WriteLine($"Successfully uploaded '{fileName}' to '{S3BucketName}'.");
+      return $"Successfully uploaded '{fileName}' to '{S3BucketName}'.";
     }
 
-    public async Task<MemoryStream> CompressStreamToZip(Stream inputStream, string csvFileName)
+
+    private static async Task<MemoryStream> CompressStreamToZip(Stream inputStream, string csvFileName)
     {
       inputStream.Position = 0;
-
       MemoryStream zipStream = new();
 
       using (var archive = new ZipArchive(zipStream, ZipArchiveMode.Create, leaveOpen: true))
       {
         var zipEntry = archive.CreateEntry(csvFileName, CompressionLevel.Optimal);
-
-        using (var zipEntryStream = zipEntry.Open())
-        {
-          await inputStream.CopyToAsync(zipEntryStream);
-        }
+        await using var zipEntryStream = zipEntry.Open();
+        await inputStream.CopyToAsync(zipEntryStream);
       }
 
       zipStream.Position = 0;
-
       return zipStream;
     }
   }
--- functions/kaggle/Handler.cs
+++ functions/kaggle/Handler.cs
@@ -3,6 +3,8 @@ using System.Text;
 using Amazon.S3;
 using Amazon.S3.Model;
 
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
 namespace functions.kaggle;
 public class HandlerFunction
 {
@@ -16,18 +18,6 @@ public class HandlerFunction
   private static readonly HttpClient client;
   private static readonly AmazonS3Client s3Client;
 
-
-  public async Task<string> KaggleDownload()
-  {
-    string url = $"{BaseUrl}/datasets/download/{KaggleUserSlug}/{KaggleDatasetSlug}";
-
-    HttpResponseMessage response = await client.GetAsync(url);
-    await HandleResponse(response);
-
-    using var memoryStream = await CopyContentToMemoryStream(response);
-
-    return await PutFileS3(memoryStream, OutputFileName);
-  }
   // Static constructor for initializing static resources once, when the class is first loaded
   static HandlerFunction()
   {
@@ -37,61 +27,48 @@ public class HandlerFunction
     s3Client = new AmazonS3Client();
   }
 
+
   // Actual instance constructor
   public HandlerFunction()
   {
-    if (!string.IsNullOrEmpty(KagglePersonalUser) && !string.IsNullOrEmpty(KagglePersonalKey))
-    {
-      string credentials = $"{KagglePersonalUser}:{KagglePersonalKey}";
-      string authToken = Convert.ToBase64String(Encoding.ASCII.GetBytes(credentials));
-      client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authToken);
-    }
-    else
-    {
-      Console.WriteLine("Instance constructor: Environment variables for Kaggle are missing.");
-      throw new Exception("Environment variables for `KagglePersonalUser` or `KagglePersonalKey` are missing.");
-    }
+    var authToken = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{KagglePersonalUser}:{KagglePersonalKey}"));
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authToken);
   }
 
-  private static async Task HandleResponse(HttpResponseMessage response)
+
+  public async Task<string> KaggleDownload()
   {
-    if (response.IsSuccessStatusCode) return;
+    string url = $"{BaseUrl}/datasets/download/{KaggleUserSlug}/{KaggleDatasetSlug}";
+    HttpResponseMessage response = await client.GetAsync(url);
+    using var memoryStream = await CopyReponseToMemoryStream(response);
 
-    string errorContent = await response.Content.ReadAsStringAsync();
-    Console.WriteLine($"Error Response Content: {errorContent}");
-
-    throw new HttpRequestException($"Request to Kaggle API failed with status code {response.StatusCode} for URL: {response.RequestMessage.RequestUri}. Error Content: {errorContent}");
+    return await PutFileS3(memoryStream, OutputFileName);
   }
 
-  private static async Task<MemoryStream> CopyContentToMemoryStream(HttpResponseMessage response)
+
+  private static async Task<MemoryStream> CopyReponseToMemoryStream(HttpResponseMessage response)
   {
-    var memoryStream = new MemoryStream();
+    MemoryStream memoryStream = new();
     await response.Content.CopyToAsync(memoryStream);
     memoryStream.Position = 0;
 
     return memoryStream;
   }
 
+
   private async Task<string> PutFileS3(Stream stream, string fileName)
   {
-    try
+    var request = new PutObjectRequest
     {
-      var request = new PutObjectRequest
-      {
-        BucketName = S3BucketName,
-        Key = fileName,
-        InputStream = stream,
-        ContentType = "application/zip"
-      };
+      BucketName = S3BucketName,
+      Key = fileName,
+      InputStream = stream,
+      ContentType = "application/zip"
+    };
 
-      PutObjectResponse response = await s3Client.PutObjectAsync(request);
-      Console.WriteLine($"Successfully uploaded file to S3 with HTTP status code: {response.HttpStatusCode}");
-      return $"Successfully uploaded dataset: '{KaggleDatasetSlug}' to S3 bucket: '{S3BucketName}' with the file key: '{fileName}'.";
-    }
-    catch (AmazonS3Exception ex)
-    {
-      Console.WriteLine($"Error uploading file to S3: {ex.Message}");
-      throw;
-    }
+    await s3Client.PutObjectAsync(request);
+
+    Console.WriteLine($"Successfully uploaded '{fileName}' to '{S3BucketName}'.");
+    return $"Successfully uploaded '{fileName}' to '{S3BucketName}'.";
   }
 }
